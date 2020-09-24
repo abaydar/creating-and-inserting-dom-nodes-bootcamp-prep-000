cacf4923ac7b3041da3f4e53b76ffcaf71507c58
Amanda Baydar <amandabaydar@gmail.com>
2020-09-24 15:03:57 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+
+element.innerHTML = 'Hello, DOM!';
+"Hello, DOM!"
+element.style.backgroundColor = '#f9f9f9';
+
+element.style.fontSize = 15
+15
+document.body.appendChild(element);
+<div style=​"background-color:​ rgb(249, 249, 249)​;​">​Hello, DOM!​</div>​
+element.style.textAlign = 'center';
+"center"
